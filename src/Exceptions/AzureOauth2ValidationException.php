0e286f5fddb1c030357a4da1db4d3ccfccf437d2
Adli I. Ifkar <adly.shadowbane@gmail.com>
2021-10-05 11:12:08 +0700

    Customize error

--- src/Exceptions/AzureOauth2ValidationException.php
+++ src/Exceptions/AzureOauth2ValidationException.php
@@ -23,46 +23,13 @@ class AzureOauth2ValidationException extends \Exception
     }
 
     /**
-     * Thrown when empty Client ID detected.
+     * Thrown when empty Tenant ID detected.
      *
      * @return static
      */
     #[Pure]
-    public static function clientIdIsEmpty(): self
+    public static function invalidClaim(): self
     {
-        return new static('Client ID is Empty');
-    }
-
-    /**
-     * Thrown when empty Client Secret detected.
-     *
-     * @return static
-     */
-    #[Pure]
-    public static function clientSecretIsEmpty(): self
-    {
-        return new static('Client Secret is Empty');
-    }
-
-    /**
-     * Thrown when empty Cache Name detected.
-     *
-     * @return static
-     */
-    #[Pure]
-    public static function cacheNameEmpty(): self
-    {
-        return new static('Cache Name is Empty');
-    }
-
-    /**
-     * Thrown when empty Cache Tag detected.
-     *
-     * @return static
-     */
-    #[Pure]
-    public static function cacheTagEmpty(): self
-    {
-        return new static('Cache Tag is Empty');
+        return new static('Invalid Claim. Token might be invalid or malformed.');
     }
 }
