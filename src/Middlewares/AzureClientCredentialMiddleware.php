edea2363888c1efe718abaebf88be944d8153a09
Adli I. Ifkar <adly.shadowbane@gmail.com>
2021-10-05 10:59:31 +0700

    Added middleware

---
+++ src/Middlewares/AzureClientCredentialMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Yayasanvitka\AzureOauth2Validator\Middlewares;
+
+use Closure;
+use Illuminate\Validation\UnauthorizedException;
+use Yayasanvitka\AzureOauth2Validator\AzureOauth2ClientCredentialValidator;
+
+/**
+ * Class AzureClientCredentialMiddleware.
+ *
+ * @package Yayasanvitka\AzureOauth2Validator\Middlewares
+ */
+class AzureClientCredentialMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        if (blank($request->bearerToken())) {
+            throw new UnauthorizedException('Token is not supplied', 403);
+        }
+
+        try {
+            (new AzureOauth2ClientCredentialValidator($request->bearerToken()))->validate();
+        } catch (\Exception $exception) {
+            throw new UnauthorizedException($exception->getMessage(), 403, $exception);
+        }
+
+        return $next($request);
+    }
+}
